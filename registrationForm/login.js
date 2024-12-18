2e32e71ee6a5685e44a456a2bbaae8af42f399d4
James gitere <sasmuypwa2019.com>
2024-12-18 10:18:40 +0300

    created a responsive registration form

---
+++ registrationForm/login.js
@@ -0,0 +1,119 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const togglePasswordIcon = document.getElementById("toggle-password");
+  const loginPasswordInput = togglePasswordIcon.previousElementSibling;
+  const loginUsernameError = document.querySelector(".username-error");
+  const loginPasswordError = document.querySelector(".password-error");
+  const loginUsernameInput = document.getElementById("login-username");
+  const submitLoginFormBtn = document.querySelector("#submit-login");
+  const feedBackMessage = document.querySelector("#message");
+  const toSignUpPage = document.querySelector(".signup-link p a");
+
+  toSignUpPage.addEventListener("click", () => {
+    window.location.href = "signup.html";
+  });
+
+  function togglePasswordVisibility() {
+    if (togglePasswordIcon.classList.contains("fa-eye-slash")) {
+      togglePasswordIcon.classList.remove("fa-eye-slash");
+      togglePasswordIcon.classList.add("fa-eye");
+      loginPasswordInput.type = "text";
+    } else {
+      togglePasswordIcon.classList.remove("fa-eye");
+      togglePasswordIcon.classList.add("fa-eye-slash");
+      loginPasswordInput.type = "password";
+    }
+  }
+
+  const response = JSON.stringify({
+    success: true,
+    
+  });
+
+  function fetchLoginResponse() {
+    console.log(response);
+    const data = JSON.parse(response);
+    return data;
+  }
+  function displayMessage(data) {
+    if (data.success) {
+      feedBackMessage.classList.add("success");
+      feedBackMessage.textContent = "Success!";
+      setTimeout(() => {
+        feedBackMessage.classList.remove("success");
+        feedBackMessage.textContent = "";
+        window.location.href = "home.html";
+      }, 2000);
+    } else {
+      if (data.reason === "password") {
+        feedBackMessage.classList.add("error");
+        feedBackMessage.textContent = "Wrong Password";
+        loginPasswordInput.style.border = "2px solid red";
+      } else if (data.reason === "username") {
+        feedBackMessage.classList.add("error");
+        feedBackMessage.textContent = "Wrong Username";
+        loginUsernameInput.style.border = "2px solid red";
+      } else {
+        feedBackMessage.classList.add("error");
+        feedBackMessage.textContent = "Something went wrong";
+      }
+      setTimeout(() => {
+        feedBackMessage.classList.remove("error");
+        feedBackMessage.textContent = "";
+      }, 2000);
+    }
+  }
+  function submitLoginForm(event) {
+    event.preventDefault();
+
+    const username = loginUsernameInput.value;
+    const password = loginPasswordInput.value;
+
+    if (!username) {
+      validateInput(loginUsernameInput, loginUsernameError, validateUsername);
+      loginUsernameError.style.display = "block";
+    }
+    if (!password) {
+      validateInput(loginPasswordInput, loginPasswordError, validatePassword);
+      loginPasswordError.style.display = "block";
+    }
+    if (username && !validateUsername(username)) {
+      validateInput(loginUsernameInput, loginUsernameError, validateUsername);
+      loginUsernameError.style.display = "block";
+    }
+    if (password && !validatePassword(password)) {
+      validateInput(loginPasswordInput, loginPasswordError, validatePassword);
+      loginPasswordError.style.display = "block";
+    }
+
+    if (validateUsername(username) && validatePassword(password)) {
+      console.log("username and password are valid");
+      let data;
+      try {
+        data = fetchLoginResponse();
+      } catch (error) {
+        console.log(error);
+        data = {
+          success: false,
+          reason: "error",
+        };
+      }
+      displayMessage(data);
+    }
+  }
+
+  submitLoginFormBtn.addEventListener("click", submitLoginForm);
+
+  togglePasswordIcon.addEventListener("click", togglePasswordVisibility);
+  loginUsernameInput.addEventListener("focus", () => {
+    validateInput(loginUsernameInput, loginUsernameError, validateUsername);
+  });
+  loginUsernameInput.addEventListener("input", () => {
+    validateInput(loginUsernameInput, loginUsernameError, validateUsername);
+  });
+  loginPasswordInput.addEventListener("focus", () => {
+    validateInput(loginPasswordInput, loginPasswordError, validatePassword);
+  });
+  loginPasswordInput.addEventListener("input", () => {
+    validateInput(loginPasswordInput, loginPasswordError, validatePassword);
+  });
+});
---
+++ registrationForm/signup.js
@@ -0,0 +1,120 @@
+const passwordToggleInput = document.querySelector(
+  "fieldset:nth-of-type(3) > span"
+);
+
+const main = document.querySelector("main");
+const toLogInPage = document.querySelector('header p a');
+
+const submitBtn = document.querySelector("#submit");
+const submitModal = document.querySelector("#submit-modal");
+const showHideToggle = document.querySelector("#toggle-password");
+
+const emailInput = document.querySelector('input[type="email"]');
+const emailError = document.querySelector("#error-email");
+
+const usernameInput = document.querySelector('input[type="text"]');
+const usernameError = document.querySelector("#error-username");
+
+const passwordInput = document.querySelector('input[type="password"]');
+const passwordError = document.querySelector("#error-password");
+
+toLogInPage.addEventListener("click", () => {
+  window.location.href = "login.html";
+})
+
+function togglePasswordVisibility() {
+  if (showHideToggle.className === "show") {
+    showHideToggle.textContent = "Hide";
+    showHideToggle.className = "hide";
+    passwordInput.type = "text";
+  } else if (showHideToggle.className === "hide") {
+    passwordInput.type = "password";
+    showHideToggle.textContent = "Show";
+    showHideToggle.className = "show";
+  }
+}
+
+function showSubmitModal() {
+  submitModal.classList.add("popup-display");
+  main.style.display = "none";
+}
+function removeSubmitModal() {
+  submitModal.classList.remove("popup-display");
+  window.location.replace("login.html");
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  if (!emailInput.value) {
+    validateInput(emailInput, emailError, validateEmail);
+    emailError.style.display = "block";
+  }
+  if (!usernameInput.value) {
+    validateInput(usernameInput, usernameError, validateUsername);
+    usernameError.style.display = "block";
+  }
+  if (!passwordInput.value) {
+    validateInput(passwordInput, passwordError, validatePassword);
+    passwordError.style.display = "block";
+  }
+  if (passwordInput.value && usernameInput.value && emailInput.value) {
+
+    showSubmitModal();
+  }
+
+}
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validateUsername(username) {
+  const usernameRegex = /^[a-zA-Z0-9!@#$%^&*()_+]{3,16}$/;
+
+  return usernameRegex.test(username);
+}
+
+function validatePassword(password) {
+  const passwordRegex =
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordRegex.test(password);
+}
+
+function validateInput(inputElement, errorElement, validationFuction) {
+  const inputValue = inputElement.value.trim();
+  if (validationFuction(inputValue)) {
+    inputElement.className = "valid";
+    errorElement.style.display = "none";
+
+    inputElement.style.border = "2px solid green";
+  } else {
+    inputElement.className = "invalid";
+
+    inputElement.style.border = "2px solid red";
+  }
+}
+
+emailInput.addEventListener("focus", () => {
+  validateInput(emailInput, emailError, validateEmail);
+});
+emailInput.addEventListener("input", () => {
+  validateInput(emailInput, emailError, validateEmail);
+});
+usernameInput.addEventListener("focus", () => {
+  validateInput(usernameInput, usernameError, validateUsername);
+});
+usernameInput.addEventListener("input", () => {
+  validateInput(usernameInput, usernameError, validateUsername);
+});
+passwordInput.addEventListener("focus", () => {
+  validateInput(passwordInput, passwordError, validatePassword);
+});
+passwordInput.addEventListener("input", () => {
+  validateInput(passwordInput, passwordError, validatePassword);
+});
+
+passwordToggleInput.addEventListener("click", togglePasswordVisibility);
+document
+  .querySelector(".popup button")
+  .addEventListener("click", removeSubmitModal);
+submitBtn.addEventListener("click", submitForm);
